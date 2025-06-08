dfbd47c41bb75b76b6f6732fc3ef5941392e1991
DemaPy <benderdema@gmail.com>
2025-06-08 21:43:14 +0200

    feat: frontend initialized

---
+++ frontend/src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import PostList from "./components/PostsList.vue";
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+const posts = ref<Post[]>([]);
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => response.json())
+  .then((json) => (posts.value = json));
+</script>
+
+<template>
+  <PostList :posts="posts" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ frontend/src/components/PostsList.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+defineProps<{ posts: Post[] }>();
+</script>
+
+<template>
+  <h1>Posts</h1>
+  <ul class="flex flex-col gap-2">
+    <li
+      class="rounded-md items-start text-left border px-4 py-2 border-gray-200 flex flex-col gap-2"
+      v-for="post in posts"
+    >
+      <p class="capitalize">{{ post.title }}</p>
+      <p>{{ post.body }}</p>
+    </li>
+  </ul>
+</template>
